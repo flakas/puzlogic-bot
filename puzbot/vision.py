347105bc1f5458cd87ba0ed9ddfb1c9d0769d225
Tautvidas Sipavičius <t.9b5as@gmail.com>
2018-11-06 23:52:34 +0200

    Clarify vision code explaining more what each section does

--- puzbot/vision.py
+++ puzbot/vision.py
@@ -5,22 +5,6 @@ import pytesseract
 from PIL import Image
 from mss import mss
 
-"""
-    - Detect game window
-    - Create different encapsulated sources for images - either a screenshot, or a file
-    - detect start screen of the game ("Press any key")
-    - detect level selection screen ("play")
-        * Detect individual level locations
-        * Detect where a level needs to be dragged
-    - Level screen
-        * Detect cells in board
-        * Parse numbers within cells
-        * Parse available pieces
-    - Detect "Level completed" screen
-        * Find location of "Next" button
-
-"""
-
 class ImageFileSource:
     def __init__(self, path):
         self.path = path
@@ -41,12 +25,12 @@ class ScreenshotSource:
         return self.image
 
     def refresh(self):
-        sct_img = self.screen.grab(self.monitor)
-        img = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
-        img = np.array(img)
-        img = self.convert_rgb_to_bgr(img)
+        source_image = self.screen.grab(self.monitor)
+        rgb_image = Image.frombytes('RGB', source_image.size, source_image.rgb)
+        rgb_image = np.array(rgb_image)
+        bgr_image = self.convert_rgb_to_bgr(rgb_image)
 
-        return img
+        return bgr_image
 
     def convert_rgb_to_bgr(self, img):
         return img[:, :, ::-1]
@@ -73,35 +57,31 @@ class Vision:
 
     @cache_until_refresh
     def get_game_board(self):
-        """ Detects location of the game board and returns it """
-        img = self.source.get()
-        img_final = img.copy()
-        img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        """ Detects the game window area within a computer screen """
+
+        screen_image = self.source.get()
+        original_screen_image = screen_image.copy()
+        grayscale = cv2.cvtColor(screen_image, cv2.COLOR_BGR2GRAY)
 
         # Find black background around the game screen
-        ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
-        new_img = cv2.bitwise_not(mask)
-        '''
-            Remove noisy portion
-        '''
-        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,
-                                                             3))  # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more
-        dilated = cv2.dilate(new_img, kernel, iterations=1)  # dilate , more the iteration more the dilation
+        ret, mask = cv2.threshold(grayscale, 1, 255, cv2.THRESH_BINARY)
+        binary_grayscale = cv2.bitwise_not(mask)
 
-        im2, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)  # get contours
+        # Eliminate noise and smaller elements
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        dilated = cv2.dilate(binary_grayscale, kernel, iterations=1)
+
+        _, contours, _ = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
             # get rectangle bounding contour
             [x, y, w, h] = cv2.boundingRect(contour)
 
-            # Discard small pieces, we're looking for a game window 800x800
+            # Discard small pieces, we're looking for a game window roughly 800x600
             if w < 700 or h < 500 or w > 800:
                 continue
 
-            # draw rectangle around contour on original image
-            # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
-            cropped = img_final[y:y+h, x:x+w]
+            cropped = original_screen_image[y:y+h, x:x+w]
 
             return (x, y, w, h, cropped)
 
@@ -110,82 +90,65 @@ class Vision:
     @cache_until_refresh
     def get_pieces(self):
         cells = self.get_visible_cells()
-
         lowest_cell = max(cells, key=lambda c: c[1])
 
+        # Expect available pieces to be lower than the lowest row of game board cells
         return list(filter(lambda c: abs(lowest_cell[1] - c[1]) < lowest_cell[3]*3, cells))
 
     @cache_until_refresh
     def get_cells(self):
         cells = self.get_visible_cells()
-
         pieces = self.get_pieces()
 
         return list(set(cells) - set(pieces))
 
     @cache_until_refresh
     def get_visible_cells(self):
-        (x, y, w, h, img) = self.get_game_board()
+        (_, _, _, _, game_screen) = self.get_game_board()
 
-        img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        grayscale = cv2.cvtColor(game_screen, cv2.COLOR_BGR2GRAY)
 
-        ret, mask = cv2.threshold(img2gray, 100, 255, cv2.THRESH_BINARY)
-        new_img = cv2.bitwise_not(mask)
+        ret, mask = cv2.threshold(grayscale, 100, 255, cv2.THRESH_BINARY)
+        binary_grayscale = cv2.bitwise_not(mask)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,
-                                                             3))  # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more
-        dilated = cv2.dilate(new_img, kernel, iterations=1)  # dilate , more the iteration more the dilation
+        # Erase smaller artifacts on screen, hopefully leaving only
+        # larger contours to detect.
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        dilated = cv2.dilate(binary_grayscale, kernel, iterations=1)
 
-        im2, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # get contours
+        _, contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # [x, y, w, h, img]
-        contours = map(lambda c: list(cv2.boundingRect(c)), contours)
-        candidates = filter(lambda c: 40 < c[2] < 60 and 40 < c[3] < 60, contours)
-        cells = map(lambda c: tuple(list(c[0:4]) + [self._recognize_number(img[c[1]:c[1]+c[3], c[0]:c[0]+c[2]])]), candidates)
+        bounding_boxes = map(lambda c: list(cv2.boundingRect(c)), contours)
+        candidates = filter(lambda b: 40 < b[2] < 60 and 40 < b[3] < 60, bounding_boxes)
+        cells = map(lambda c: tuple(list(c[0:4]) + [self._recognize_number(game_screen[c[1]:c[1]+c[3], c[0]:c[0]+c[2]])]), candidates)
         return list(cells)
-
-        # for contour in candidates:
-            # [x, y, w, h, c] = contour
-            # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
-        # return img
 
     @cache_until_refresh
     def get_constraints(self):
+        # TODO: add constraint identification
         return []
 
-    def _recognize_number(self, img):
-        img = img[5:-5, 5:-5]
-        img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def _recognize_number(self, candidate_tile_image):
+        """ Attempts to OCR the number within a game tile image """
 
-        ret, mask = cv2.threshold(img2gray, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        new_img = cv2.bitwise_not(mask)
+        borderless_image = candidate_tile_image[5:-5, 5:-5]
+        grayscale = cv2.cvtColor(borderless_image, cv2.COLOR_BGR2GRAY)
 
-        new_img = cv2.medianBlur(new_img, 3)
+        ret, mask = cv2.threshold(grayscale, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        binary_grayscale = cv2.bitwise_not(mask)
 
-        # Turn white text \w black background into black text \w white background
-        if new_img[0, 0] == 0:
-            new_img = cv2.bitwise_not(new_img)
+        # Blur to help text show up better for OCR
+        ocr_image = cv2.medianBlur(binary_grayscale, 3)
 
-        # cv2.imshow('grayscale', new_img)
-        # cv2.waitKey()
-        # print(pytesseract.image_to_string(new_img, config='--psm 10'))
-        character = pytesseract.image_to_string(new_img, config='--psm 10')
+        if ocr_image[0, 0] == 0:
+            # OCR needs black text on white background
+            black_text_on_white_background = cv2.bitwise_not(ocr_image)
+            ocr_image = black_text_on_white_background
+
+        # Use single-character segmentation mode for Tesseract
+        character = pytesseract.image_to_string(ocr_image, config='--psm 10')
         try:
             return int(character)
         except:
-            # print(character)
             return False
-
-    def debug(self):
-        start = time.time()
-        # res = self.get_pieces()
-        # cv2.imshow('grayscale', res)
-        # cv2.waitKey()
-        for cell in self.get_pieces():
-            print(cell)
-
-        for cell in self.get_cells():
-            print(cell)
-
-        print('Ended at', time.time() - start)
